9fc6709910ea5f9c2453f1c841d9b0886a844365
Itozee <136532526+Itozee@users.noreply.github.com>
2023-08-09 18:45:48 -0400

    Create Update statements.sql

---
+++ Code/Update statements.sql
@@ -0,0 +1,36 @@
+```
+-- Update in a column 
+UPDATE cars_dim
+SET MODEL = 'XE'
+WHERE CarKey = '2017Maruti Ciaz';
+
+-- Update in a column 
+UPDATE cars_dim
+SET MODEL = 'LXI'
+WHERE CarKey = '2013Maruti Swift Dzire';
+
+-- Update in a column 
+UPDATE cars_dim
+SET MODEL = 'ZDi Automatic'
+WHERE CarKey = '2019Maruti Vitara Brezza';
+
+-- Update in a column 
+UPDATE cars_dim
+SET MODEL = 'Sigma'
+WHERE CarKey = '2017Maruti Ciaz';
+
+-- Update in a column 
+UPDATE cars_dim
+SET MODEL = 'ALPHA MT'
+WHERE CarKey = '2016Maruti Baleno';
+
+-- Update in a column 
+UPDATE cars_dim
+SET MODEL = 'i20'
+WHERE CarKey = '2016Hyundai Elite i20';
+
+-- Update in a column 
+UPDATE cars_dim
+SET MODEL = 'RXE'
+WHERE CarKey = '2021Renault TRIBER';
+```
